b50ee4c41a90c1409afd941c00bf651fb6ab3aa2
Pedro Manuel Gómez-Portillo <pedroma.almagro@gmail.com>
2019-04-12 13:22:09 +0200

    Addding correation functions

--- src/main.R
+++ src/main.R
@@ -8,6 +8,10 @@ setwd("~/GitHub/SIGE/")
 library(tidyverse)
 library(dplyr)
 library(funModeling)
+library(corrplot)
+library(Hmisc)
+library(ggplot2)
+
 options(max.print = 999)
 
 set.seed(1)
@@ -23,27 +27,60 @@ summary(data_raw)
 
 status <- df_status(data_raw) # ???con esto sabemos que hay columnas con un 0.02% de NA
 
-# glimpse(data_raw)
+glimpse(data_raw)
 
 
-# 2. Eliminación de columnas no útiles -----------------------------
+# 2. Eliminación de columnas y filas no útiles -----------------------------
 
-## Identificar columnas con más del 0.01% de los valores a NA (aunque actualmente no exista ninguno)
+## Eliminar la columna ID por no aportar nada
+data <- select(data_raw, -ID_code)
 
-na_cols <- status %>%
-  filter(p_na > 0.01) %>%
-  select(variable)
+## Eliminar las filas con valores NA (https://stackoverflow.com/a/4862264/3594238)
+data <- na.omit(data)
 
-## Eliminarlas
+## Es imposible saber de momento si el resto de columnas son útiles o no, así que vamos
+## a intentar averiguarlo
 
-remove_cols <- bind_rows(
-  list(
-    na_cols
-  )
-)
+## Calcular la correlación entre las variables
+# cor_matrix <- cor(data, use = "complete.obs")
+# round(cor_matrix, 2)
 
-## Actualizar el dataset
+cor_matrix <- rcorr(as.matrix(data))
+corrplot(cor_matrix$r, type = "upper", order = "hclust",  p.mat = cor_matrix$P, sig.level = 0.01, insig = "blank")
 
-data <- data_raw %>%
-  select(-one_of(remove_cols$variable))
+## Usar un mapa de calor (no vale pa na)
+# palette <- colorRampPalette(c("blue", "white", "red"))(20)
+# heatmap(x = cor_matrix$r, col = palette, symm = TRUE)
 
+
+## Ahora borraremos las columnas que tengan menos de un 0.02% de correlacion con la columna target
+# data <- data %>%
+#   mutate(target = case_when(
+#     target == 0  ~ 'Yes',
+#     target == 1 ~ 'No'))
+
+data_num <- data %>%
+  na.exclude() %>%
+  mutate_if(is.character, as.factor) %>%
+  mutate_if(is.factor, as.numeric)
+
+cor_target <- correlation_table(data_num, target='target')
+important_vars <- cor_target %>% 
+  filter(abs(target) >= 0.05)
+
+## Cuando tenemos las columnas las seleccionamos del dataset
+data_num <- data_num %>%
+  select(one_of(important_vars$Variable))
+
+## Alta correlacion entre sí
+rcorr_result <- rcorr(as.matrix(data_num))
+cor_matrix <- as_tibble(rcorr_result$r, rownames = "variable")
+corrplot(rcorr_result$r, type = "upper", order = "original", tl.col = "black", tl.srt = 45)
+
+## Representarlo en un diagrama
+v <- varclus(as.matrix(data_num), similarity="pearson") 
+plot(v)
+
+write_csv(data_num, 'data/processed.csv')
+
+## Eliminar ruido
